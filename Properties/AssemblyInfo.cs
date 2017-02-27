385c3d11b094d576b9e8bbd19767d35ad97e1d87
LiteHell <LiteHell@openmailbox.org>
2017-02-27 20:32:28 +0900

    Added copyright info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("LibHitomi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library about hitomi.la")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LibHitomi")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © LiteHell 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
